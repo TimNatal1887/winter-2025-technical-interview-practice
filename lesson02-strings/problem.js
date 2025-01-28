ea86b2b7c5f304db70a52ecf6e71cfc255bfa07d
Timothy Natal <timothynatal@pursuit.org>
2025-01-28 11:46:27 -0500

    lesson2 problem solved

--- lesson02-strings/problem.js
+++ lesson02-strings/problem.js
@@ -5,7 +5,42 @@
  * @return {number}
  */
 function lengthOfLongestSubstring(s) {
-  // Your code here
-}
+  let currentSubString = []
 
+  const separatedSubstrings = []
+  for(let i = 0;i<s.length; i++){
+    let currentChar = s[i]
+    if(!currentSubString.includes(currentChar)){
+      currentSubString.push(currentChar)
+    }else{
+      const newSubString = currentSubString.join("")
+      separatedSubstrings.push(newSubString)
+      currentSubString = []
+      currentSubString.push(currentChar)
+    }
+    if(i=== s.length - 1){
+      const newSubString = currentSubString.join("")
+      separatedSubstrings.push(newSubString)
+    }
+  }  
+
+  
+  const longestSubString = separatedSubstrings.reduce((currentLongest, currentString) =>{
+    if(currentLongest.length < currentString.length){
+      currentLongest = currentString
+    }
+    return currentLongest
+  },'')
+
+  // let longestSubstring = ''
+  // for(let i = 0; i < separatedSubstrings.length; i++){
+  //   let currentString = separatedSubstrings[i]
+
+  //   if(longestSubString.length < currentString.length){
+  //     longestSubString = currentString
+  //   }
+  // }
+  return longestSubString.length
+}
+console.log(lengthOfLongestSubstring("hello my name is Timothy"))
 module.exports = lengthOfLongestSubstring;
--- lesson02-strings/warmup.js
+++ lesson02-strings/warmup.js
@@ -1,15 +1,19 @@
 // 1. given a string with three characters, return the second character
-
+    function getSecondChar(str){
+        return str.charAt(1)
+    }
 // 2. get the second and third char from a string. the string will always have atleast three characters
-
+    function getSecondAndThirdChar(str){
+        return str.substring(1,3)
+    }
 // 'cat' => 'at'
 // 'robbed' => 'ob'
 
 // 3. Given a word with 5 letters, return the second through 5th letter
 
-// function getSecondThroughFifthLetter (word){
-//   return word.slice(1);
-// }
+function getSecondThroughFifthLetter (word){
+  return word.slice(1);
+}
 
 // 4. given a string and a number called N, return the second through the Nth character
 
