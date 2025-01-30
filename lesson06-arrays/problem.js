be5c103dc8f439de71712709ad5a06e8c684715d
Timothy Natal <timothynatal@pursuit.org>
2025-01-30 13:36:55 -0500

    lesson6 problem

--- lesson06-arrays/problem.js
+++ lesson06-arrays/problem.js
@@ -5,6 +5,17 @@
  * @param {number[]} nums - The integer array.
  * @returns {number} - The index of the largest element if it meets the condition, otherwise -1.
  */
-function checkLargestElement(nums) {}
+function checkLargestElement(nums) {
+    if(nums.length < 1) return -1;
 
+    const largestNum = Math.max(...nums);  
+    const numsWithoutLargest = nums.filter((num)=> num !== largestNum);
+    const secondLargest = Math.max(...numsWithoutLargest);
+    
+    if(largestNum >= 2 * secondLargest){
+      return largestNum
+    }
+    
+    return -1
+}
 module.exports = checkLargestElement;
--- lesson06-arrays/problem.test.js
+++ lesson06-arrays/problem.test.js
@@ -2,8 +2,8 @@ const checkLargestElement = require("./problem");
 
 describe("checkLargestElement", () => {
   test("should return the largest element in an array of positive numbers", () => {
-    const nums = [1, 5, 3, 9, 2];
-    expect(checkLargestElement(nums)).toBe(9);
+    const nums = [1, 5, 3, 11, 2];
+    expect(checkLargestElement(nums)).toBe(11);
   });
   test("should return the largest element in an array of negative numbers", () => {
     const nums = [-7, -2, -5, -1];
@@ -12,5 +12,9 @@ describe("checkLargestElement", () => {
   test("should return the largest element in an array of mixed numbers", () => {
     const nums = [-7, 5, -5, 1];
     expect(checkLargestElement(nums)).toBe(5);
+  });
+  test("should return -1 if input is an empty array",()=>{
+    const nums = []
+    expect(checkLargestElement(nums)).toBe(-1)
   });
 });
