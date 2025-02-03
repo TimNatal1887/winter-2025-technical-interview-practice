c15355deb3d73d82c2ec2ea411d9212232e7e215
Timothy Natal <timothynatal@pursuit.org>
2025-02-03 14:00:50 -0500

    lesson07 problem solved

--- lesson07-arrays/problem.js
+++ lesson07-arrays/problem.js
@@ -4,5 +4,31 @@
 // // removeDuplicates([1, 1, 1, 1, 1, 1]); // => [1]
 // // removeDuplicates([1, 2, 3, 4, 5]); // => [1, 2, 3, 4, 5]
 module.exports = function removeDuplicates(arr) {
-  // your code here
+  // SOLUTION 1:
+    // const arrayWithoutDuplicates = []
+    // for(let i = 0; i < arr.length;i++){
+    //   const currentNum = arr[i]
+    //   if(!arrayWithoutDuplicates.includes(currentNum)){
+    //     arrayWithoutDuplicates.push(currentNum)
+    //   }
+    // }
+
+    // return arrayWithoutDuplicates
+
+
+  // SOLUTION 2:
+  // return Array.from(new Set(arr)) 
+
+  // SOLUTION 3:
+  const arrayWithoutDuplicates = arr.reduce((finalArr, currentNum) =>{
+    if(!finalArr.includes(currentNum)){
+      finalArr.push(currentNum)
+    }
+
+    return finalArr
+  },[])
+
+  return arrayWithoutDuplicates
+
 };
+
